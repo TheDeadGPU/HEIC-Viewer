59ca193456b17c5f5676626c7ff295aad10aa21e
James Girard <girardj@go.stockton.edu>
2021-04-03 12:19:58 -0400

    Added File Menu and Open HEIC File Dialog

--- HEICViewer/MainWindow.axaml.cs
+++ HEICViewer/MainWindow.axaml.cs
@@ -6,14 +6,17 @@ using Avalonia.Media;
 using Avalonia.Media.Imaging;
 using ImageMagick;
 using System;
+using System.Collections.Generic;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace HEICViewer
 {
     public class MainWindow : Window
     {
+        MenuItem item;
         public MainWindow()
         {
             InitializeComponent();
@@ -25,13 +28,17 @@ namespace HEICViewer
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
-            ImageLoad();
-        }
-        void ImageLoad()
-        {
+            item = this.Get<MenuItem>("OpenFile");
+            item.Click += Item_Click;
             string[] ARGS = Environment.GetCommandLineArgs();
-            Console.WriteLine("OUT: " + ARGS[1]);
-            MagickImage image = new MagickImage(ARGS[1]);
+            if (ARGS.Length > 1)
+            {
+                ImageLoad(ARGS[1]);
+            }
+        }
+        void ImageLoad(string path)
+        {
+            MagickImage image = new MagickImage(path);
             image.Format = MagickFormat.Jpeg;
             MemoryStream tmpstream = new MemoryStream();
             image.Write(tmpstream);
@@ -39,6 +46,24 @@ namespace HEICViewer
             Avalonia.Media.Imaging.Bitmap amib = new Avalonia.Media.Imaging.Bitmap(tmpstream);
             Image img = this.Get<Image>("ImageView");
             img.Source = amib;
+        }
+        private void Item_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            OpenHEICFileDialog();
+        }
+        private async Task OpenHEICFileDialog()
+        {
+            var dlg = new OpenFileDialog();
+            dlg.Filters.Add(new FileDialogFilter() { Name = "HEIC (.heic)", Extensions = { "heic" } });
+            dlg.Filters.Add(new FileDialogFilter() { Name = "All Files", Extensions = { "*" } });
+            dlg.AllowMultiple = true;
+
+            var result = await dlg.ShowAsync(this);
+            if (result != null)
+            {
+                string[] fileNames = result;
+                ImageLoad(fileNames[0]);
+            }
         }
     }
 }
